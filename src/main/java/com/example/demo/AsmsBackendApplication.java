b0c3a5e2bcb042d04d286fdec1f2f7c67b0b3862
WeerasingheMSC <sahanchampathi2001ugc@gmail.com>
2025-09-26 02:36:25 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/AsmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmsBackendApplication.class, args);
+	}
+
+}
