13c72e73d933f5cceab91e6d12af94b6a9e23d8c
WeerasingheMSC <sahanchampathi2001ugc@gmail.com>
2025-11-02 11:10:07 +0530

    Completed employee registration

---
+++ ASMS_Backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and() // enable CORS
+                .csrf().disable() // disable CSRF for APIs
+                .authorizeHttpRequests()
+                .requestMatchers("/api/auth/**").permitAll() // allow signup/login
+                .anyRequest().authenticated(); // secure all other APIs
+
+        return http.build();
+    }
+}
---
+++ ASMS_Backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.dto.*;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> signup(@RequestBody SignupRequest request) {
+        return ResponseEntity.ok(authService.signup(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+}
